d5b8c8263b29acda0122fc0acccbd6313eccd1c5
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-02-04 23:11:10 +0600

    Conditionally blog data showing complete

--- src/pages/blog-page/Blogs.jsx
+++ src/pages/blog-page/Blogs.jsx
@@ -5,21 +5,37 @@ import SingleBlog from './SingleBlog';
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
+  const [seeMore, setSeemore] = useState(false);
   const axios = useAxiosPublic();
   useEffect(() => {
     axios.get('/blogs').then(res => {
       setBlogs(res.data);
     });
   }, [axios]);
-  console.log(blogs);
+  console.log(blogs, seeMore);
   return (
     <>
       <SecondaryBanner title="Blogs"></SecondaryBanner>
       <div className="flex ">
         <div className=" flex-1 ">
-          {blogs.map(blog => (
-            <SingleBlog key={blog._id} blog={blog}></SingleBlog>
-          ))}
+          {seeMore
+            ? blogs
+                .slice(0, 2)
+                .map(blog => (
+                  <SingleBlog key={blog._id} blog={blog}></SingleBlog>
+                ))
+            : blogs.map(blog => (
+                <SingleBlog key={blog._id} blog={blog}></SingleBlog>
+              ))}
+
+          <div className="text-center">
+            <button
+              onClick={() => setSeemore(!seeMore)}
+              className="btn bg-main my-3"
+            >
+              {seeMore ? 'See More' : 'See Less'}
+            </button>
+          </div>
         </div>
         <div className="w-96">Action part</div>
       </div>
--- src/pages/blog-page/SingleBlog.jsx
+++ src/pages/blog-page/SingleBlog.jsx
@@ -1,21 +1,40 @@
 /* eslint-disable react/prop-types */
 const SingleBlog = ({ blog }) => {
   return (
-    <div className="card card-compact max-w-5xl mx-auto bg-base-100 shadow-xl">
+    <div className="card card-compact max-w-5xl mx-auto bg-base-100 my-10 shadow-xl">
       <img className="w-full" src={blog.blogImage} alt="Blog Image" />
 
       <div className="card-body">
         <h2 className="card-title">{blog.title}</h2>
         <span>
           {blog.tags.map(tag => (
-            <button className="btn mr-3" key={tag}>
+            <button
+              className="px-5 bg-main rounded-full font-bold mr-3"
+              key={tag}
+            >
               {tag}
             </button>
           ))}
         </span>
-        <p>{blog.description}</p>
+        <p className="text-base">{blog.description}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <div className="flex gap-3 justify-end">
+            <div>
+              <p className="mt-1">
+                Writer:{' '}
+                <span className="font-semibold ">{blog.writerName}</span>
+              </p>
+              <p>
+                Published Date:{' '}
+                <span className="font-semibold ">{blog.date}</span>
+              </p>
+            </div>
+            <img
+              className="w-10 h-10 rounded-full"
+              src={blog.writerImage}
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
