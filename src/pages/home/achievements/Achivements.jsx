7bc577919465c10ee404558f1e3510f2d956f87c
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-02-08 23:52:16 +0600

    Responsive for all devices added

--- src/pages/home/achievements/Achivements.jsx
+++ src/pages/home/achievements/Achivements.jsx
@@ -1,20 +1,20 @@
 const Achievements = () => {
   return (
-    <div className="grid bg-[#08a8e4] h-[30vh] lg:grid-cols-4 gap-5">
-      <div className="shadow-xl text-center h-40 my-auto">
-        <h1 className="text-6xl font-bold">1.2k Flats</h1>
+    <div className="grid  md:grid-cols-2 bg-[#08a8e4] sm:h-[90vh] md:h-[50vh] lg:h-[40vh] lg:grid-cols-4 gap-5 my-8">
+      <div className="shadow-xl text-center h-24 lg:h-40 my-auto pt-2">
+        <h1 className=" md:text-3xl lg:text-6xl font-bold">1.2k Flats</h1>
         <p className="text-lg font-semibold">Sales Successfully</p>
       </div>
-      <div className="shadow-xl text-center h-40 my-auto">
-        <h1 className="text-6xl font-bold">2000+</h1>
+      <div className="shadow-xl text-center h-24 lg:h-40 my-auto">
+        <h1 className="md:text-3xl lg:text-6xl font-bold">2000+</h1>
         <p className="text-lg font-semibold">Customers</p>
       </div>
-      <div className="shadow-xl text-center h-40 my-auto">
-        <h1 className="text-6xl font-bold">200+</h1>
+      <div className="shadow-xl text-center h-24 lg:h-40 my-auto">
+        <h1 className="md:text-3xl lg:text-6xl font-bold">200+</h1>
         <p className="text-lg font-semibold">Current Bookings</p>
       </div>
-      <div className="shadow-xl text-center h-40 my-auto">
-        <h1 className="text-6xl font-bold">500+</h1>
+      <div className="shadow-xl text-center h-24 lg:h-40 my-auto">
+        <h1 className="md:text-3xl lg:text-6xl font-bold">500+</h1>
         <p className="text-lg font-semibold">Construction Running</p>
       </div>
     </div>
